f91b13806cdcd54a64ebf59596e846914743ae20
ggkk0818 <ggkk0818@sina.com>
2023-01-27 20:55:59 +0800

    ✨ feat(通用): 增加表与id测试代码

---
+++ sql/registration-0127.sql
@@ -0,0 +1,58 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80032
+ Source Host           : localhost:3306
+ Source Schema         : registration
+
+ Target Server Type    : MySQL
+ Target Server Version : 80032
+ File Encoding         : 65001
+
+ Date: 27/01/2023 20:39:53
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for reg_admin
+-- ----------------------------
+DROP TABLE IF EXISTS `reg_admin`;
+CREATE TABLE `reg_admin`  (
+  `id` bigint NOT NULL COMMENT '管理员id',
+  `name` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '用户名',
+  `password` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `is_enabled` tinyint(1) NULL DEFAULT NULL COMMENT '是否启用',
+  `is_del` tinyint(1) NULL DEFAULT NULL COMMENT '删除标记',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  `update_user` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '更新人id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for reg_doctor
+-- ----------------------------
+DROP TABLE IF EXISTS `reg_doctor`;
+CREATE TABLE `reg_doctor`  (
+  `id` bigint NOT NULL COMMENT '医生id',
+  `name` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '医生用户名',
+  `pwd` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `age` int NULL DEFAULT NULL COMMENT '年龄',
+  `gender` tinyint(1) NULL DEFAULT NULL COMMENT '性别（0=其他，1=男，2=女）',
+  `mobile` int NULL DEFAULT NULL COMMENT '手机号',
+  `description` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL COMMENT '医生简介',
+  `avatar` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '头像url',
+  `sur_id` varbinary(255) NULL DEFAULT NULL COMMENT '诊室id',
+  `is_enabled` tinyint(1) NULL DEFAULT NULL COMMENT '是否启用',
+  `is_del` tinyint(1) NULL DEFAULT NULL COMMENT '删除标记',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `create_user` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '创建人id',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  `update_user` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '更新人id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
---
+++ test.js
@@ -0,0 +1,7 @@
+import {snowflakeGenerator} from 'snowflake-id-js';
+
+const generator = snowflakeGenerator(512);
+
+let id = generator.next().value;
+
+console.log(typeof id)
